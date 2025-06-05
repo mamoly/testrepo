03467a00704cd250a6d8469747d79466fcf7596e
mamoly <swimultimate@gmail.com>
2025-06-06 00:39:32 +0700

    fix: enable CORS and use full URL for game creation

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h1>Shogi App</h1>
+    <button (click)="createGame()">Start Game</button>
+    <pre>{{ gameId }}</pre>
+  `
+})
+export class AppComponent {
+  gameId?: string;
+  constructor(private http: HttpClient) {}
+  createGame() {
+    const alloc = { allocations: { pawn: 50 } };
+    const url = 'http://localhost:8000/game';
+    this.http.post<any>(url, alloc).subscribe(res => this.gameId = res.game_id);
+  }
+}
