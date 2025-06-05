ec7bd2f94fb2827950d61bcff51aabe0d37f13e3
mamoly <swimultimate@gmail.com>
2025-06-06 00:38:28 +0700

    fix: enable CORS and use full URL for game creation

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -14,6 +14,7 @@ export class AppComponent {
   constructor(private http: HttpClient) {}
   createGame() {
     const alloc = { allocations: { pawn: 50 } };
-    this.http.post<any>('/game', alloc).subscribe(res => this.gameId = res.game_id);
+    const url = 'http://localhost:8000/game';
+    this.http.post<any>(url, alloc).subscribe(res => this.gameId = res.game_id);
   }
 }
